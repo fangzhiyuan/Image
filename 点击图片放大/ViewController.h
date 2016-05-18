385429e770685145746d346a3a093fd3b246645f
fangzhiyuan <fzyiphone@sina.com>
2016-05-18 15:26:53 +0800

    Initial Commit

---
+++ 点击图片放大/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  点击图片放大
+//
+//  Created by 威盛电气 on 16/5/18.
+//  Copyright © 2016年 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 点击图片放大/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  点击图片放大
+//
+//  Created by 威盛电气 on 16/5/18.
+//  Copyright © 2016年 GG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
